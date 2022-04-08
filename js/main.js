f741c3d4527cc700586d5bface72f31d4377119f
Rajasi Desai <rajasi.desai18@gmail.com>
2022-04-07 23:28:41 -0400

    add images, make changes to cart in header, and add display cart item function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 //The main file that runs everything
+
+let cartIdCounter = 0;
+let itemIdCounter = 0;
+
+function displayCartItem(itemId){
+    const newItemDiv = document.createElement("div");
+    const plusButton = document.createElement("button");
+    const minusButton = document.createElement("button");
+
+    plusButton.setAttribute("id", `plus${itemId}`);
+    minusButton.setAttribute("id", `minus${itemId}`);
+    newItemDiv.setAttribute("id", `itemDivId${itemId}`);
+
+    plusButton.appendChild(document.createTextNode("+"));
+    minusButton.appendChild(document.createTextNode("-"));
+
+    plusButton.classList.add("cartButton");
+    minusButton.classList.add("cartButton");
+
+    newItemDiv.classList.add("cartItem");
+    newItemDiv.appendChild(plusButton);
+    newItemDiv.appendChild(minusButton);
+
+    document.getElementById("currentCart").appendChild(newItemDiv);   
+}
