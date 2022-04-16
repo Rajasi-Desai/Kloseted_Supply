80796b69a2dd947b92fe118cdb205da431f137ad
amangalik@umass.edu <60450180+amangalik@users.noreply.github.com>
2022-04-16 17:45:23 -0400

    Beginning implemeting faker and added user functions

--- js/user.js
+++ js/user.js
@@ -11,7 +11,7 @@ class User {
     #cart;
 
     constructor(id, password) {
-        if (typeof id !== 'number') {
+        if (typeof id !== 'string') {
             console.error(`Cannot create user with identifier of type ${typeof id}`);
         }
         if (typeof password !== 'string') {
@@ -27,6 +27,14 @@ class User {
         return this.#id;
     }
 
+    get password() {
+        return this.#password;
+    }
+
+    get cart() {
+        return this.#cart;
+    }
+
     /** @param {string} password */
     set password(password) {
         if (typeof password !== 'string') {
@@ -36,4 +44,7 @@ class User {
     }
 }
 
+// 1. /user/register`: Register new user
+// 2. `/user/login`: Login existing user
+// 3. `/user/id/update?password=<new_password>`: Update user's password
 export {User};
