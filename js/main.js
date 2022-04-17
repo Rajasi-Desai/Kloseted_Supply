f197979ffbe1c482b7f4f2e119badebbd07c832a
gracec4227 <46689803+gracec4227@users.noreply.github.com>
2022-04-17 11:25:54 -0400

    created a fluid grid creation process using js rather than hardcoded html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //The main file that runs everything
-
-import {Cart} from "./cart";
+// import { PRODUCTS } from "./productNames.js"
+// import {Cart} from "./cart.js";
 
 let cartIdCounter = 0;
 let itemIdCounter = 0;
@@ -26,3 +26,57 @@ function displayCartItem(itemId){
 
     document.getElementById("currentCart").appendChild(newItemDiv);   
 }
+
+const PRODUCTS = ['Bar Soap',
+'Liquid Soap',
+'Shampoo',
+'Conditioner',
+'Body Wash',
+'Floss',
+'Toothbrush',
+'Toothpaste',
+'Deodorant',
+'Shaving Cream',
+'Hair Moisturizer',
+'Hair Oil',
+'Sponge',
+'Dish soap',
+'Disinfectant wipes',
+'Hand Sanitizer',
+'Lotion',
+'Band aids',
+'Lip Balm',
+'Pads',
+'Tampons',
+'Baby wipes',
+'Diaper Rash cream',
+'Diapers',
+'Toilet Paper',
+'Paper Towels',
+'Facial Tissue']
+
+function displayProductGridItem(productName){
+    const newItemDiv = document.createElement("div");
+    const name = document.createElement("p");
+    const quant = document.createElement("input");
+
+    console.log(productName)
+    name.textContent = productName
+    quant.setAttribute("type", "number");
+    quant.setAttribute("value", "0");
+
+    name.setAttribute("id", `product-listing-name-${productName}`);
+    quant.setAttribute("id",`product-listing-quant-${productName}`);
+    newItemDiv.setAttribute("id", `product-listing-div-${productName}`);
+
+    newItemDiv.classList.add("grid-item");
+    newItemDiv.appendChild(name);
+    newItemDiv.appendChild(quant);
+    document.getElementById("grid-container").appendChild(newItemDiv);
+}
+
+function build(){
+    PRODUCTS.forEach((p) => displayProductGridItem(p));
+}
+
+build()
---
+++ js/product.js
@@ -0,0 +1,25 @@
+import { PRODUCTS } from "../js/productNames.js"
+
+function displayProductGridItem(productName){
+    const newItemDiv = document.createElement("div");
+    const name = document.createElement("p");
+    const quant = document.createElement("input");
+
+    name.textContent = productName
+    quant.setAttribute("type", "number");
+    quant.setAttribute("value", "0");
+
+    name.setAttribute("id", `product-listing-name-${productName}`);
+    quant.setAttribute("id",`product-listing-quant-${productName}`);
+    newItemDiv.setAttribute("id", `product-listing-div-${productName}`);
+
+    newItemDiv.classList.add("grid-item");
+    newItemDiv.appendChild(name);
+    newItemDiv.appendChild(quant);
+    document.getElementById("grid-container").appendChild(newItemDiv);
+}
+
+function build(){
+    PRODUCTS.forEach((p) => displayProductGridItem(p));
+}
+
