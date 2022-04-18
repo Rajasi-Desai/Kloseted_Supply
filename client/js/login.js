47c77d142327fa8d83145e00084923d90d171931
amangalik@umass.edu <60450180+amangalik@users.noreply.github.com>
2022-04-17 20:21:55 -0400

    Fetching dummy users from JSON

--- client/js/login.js
+++ client/js/login.js
@@ -8,3 +8,13 @@ const usernameinput = document.getElementById("usernameinput");
 const passwordinput = document.getElementById("passwordinput");
 const rememberme = document.getElementById("rememberme");
 const loginbutton = document.getElementById("loginbutton");
+
+let response = await fetch("https://github.com/Rajasi-Desai/326-final-clockwork/blob/a4cd0b6484227f891ed011bde80001f779c86ff8/client/js/dummy-users.json");
+if(response.ok)
+{
+  var users = await response.json();
+}
+else
+{
+  alert("HTTP-Error: " + response.status);
+}
