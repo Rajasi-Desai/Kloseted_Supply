8b73a2f5ceb420208b8956b2781004c0cdc24168
gracec4227 <46689803+gracec4227@users.noreply.github.com>
2022-05-06 17:58:50 -0400

    a

--- client/js/main.js
+++ client/js/main.js
@@ -97,9 +97,6 @@ function getAllItems2() {
 async function getAllItems() {
     const response = await fetch('/getAllItems', {
       method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
     });
     console.log(response)
     return response
